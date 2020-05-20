01819e9eaa01e00850b9c77492c417d4431e4916
ShaikshaLove <44459273+ShaikshaLove@users.noreply.github.com>
2020-05-20 19:53:57 +0530

    Update OrderApiConsumer.java

--- src/main/java/io/s3soft/cartservice/api/consumer/OrderApiConsumer.java
+++ src/main/java/io/s3soft/cartservice/api/consumer/OrderApiConsumer.java
@@ -7,10 +7,10 @@ import io.s3soft.cartservice.api.dto.OrderPlaced;
 import io.s3soft.cartservice.api.dto.SuccessResponse;
 import io.s3soft.cartservice.model.Cart;
 
-@FeignClient(name="order-api-consumer",url = "http://localhost:2030/api")
+@FeignClient(name="order-api-consumer",url = "https://s3order-service.herokuapp.com")
 public interface OrderApiConsumer {
 	
-       @PostMapping("/orders")
+       @PostMapping("api/orders")
 	   public SuccessResponse<OrderPlaced> placeOrder(Cart cart) ;
 	 
 }
